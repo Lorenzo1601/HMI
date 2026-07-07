6ff83203d595468358948e7b985cf1e64d55e984
Lorenzo <lorenzo.malvogli@gmail.com>
2026-07-07 11:46:43 +0200

    Aggiunta classe Codesys per connessione PLC
    Aggiunto il namespace HMI.ExternalConnection.PLCs e la classe Codesys che eredita da PLC, con due costruttori per gestire connessioni con o senza credenziali. Importati i namespace necessari.

---
+++ ExternalConnection/PLCs/Codesys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMI.ExternalConnection.PLCs
+{
+    internal class Codesys : PLC
+    {
+        public Codesys(string IpAddress, int IpPort) : base(IpAddress, IpPort)
+        {
+        }
+        public Codesys(string IpAddress, int IpPort, string ConnectionUsername, string ConnectionPassword) : base(IpAddress, IpPort, ConnectionUsername, ConnectionPassword)
+        {
+        }
+    }
+}
